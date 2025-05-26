3579389161f294faf265103858e77d167bc698b2
Daniela Matallana Caballero <danii.matallana@gmail.com>
2025-05-26 11:31:04 -0500

    Se inserta una imagen de fondo y se realizan cambios en el color de la letra del titulo

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 import streamlit as st
 
-st.title("Demo de Componentes Básicos")
+# Para poner la imagen como fondo de pantalla
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url(https://i.ytimg.com/vi/m2AuliEdbQg/maxresdefault.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown("<h1 style='color: black;'> Instancias AWS QA</h1>", unsafe_allow_html=True)
+#st.title("Instancias AWS de QA")
+
+# Se inserta imagen
+#st.image("latinia.jpg", use_column_width=True)
+
+
 
 # Entrada de texto
 nombre = st.text_input("¿Cuál es tu nombre?")
