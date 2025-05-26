7b730dad06f54c9cb9c14d82c1295ebc6ac731f5
Daniela Matallana Caballero <danii.matallana@gmail.com>
2025-05-26 16:21:51 -0500

    Se añaden cambios para cambiar el color de los textos en la pagina

--- app.py
+++ app.py
@@ -15,20 +15,47 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.markdown("<h1 style='color: black;'> Instancias AWS QA</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='color: black;'> Instancias AWS QA </h1>", unsafe_allow_html=True)
 #st.title("Instancias AWS de QA")
 
 # Se inserta imagen
 #st.image("latinia.jpg", use_column_width=True)
 
-
+# CSS personalizado para cambiar color del texto del input
+st.markdown("<p style='color: black;'> ¿Cuál es tu nombre? "
+" </p>", unsafe_allow_html=True)
 
 # Entrada de texto
-nombre = st.text_input("¿Cuál es tu nombre?")
+nombre = st.text_input("")
 
 # Botón para mostrar saludo
 if st.button("Saludar"):
     if nombre:
-        st.success(f"¡Hola, {nombre}!")
+        #st.markdown(f"<p style='color: orange; font-size: 20px;'>¡Hola, {nombre}!</p>", unsafe_allow_html=True)
+        st.markdown(
+    f"""
+    <div style='
+        background-color: #ffeeba;
+        padding: 10px;
+        border-radius: 5px;
+        color: #856404;
+        font-weight: bold;
+    '>
+        ¡Hola, {nombre}!
+    </div>
+""", unsafe_allow_html=True)
+
     else:
-        st.warning("Por favor, escribe tu nombre.")
+       # st.warning("Por favor, escribe tu nombre.")
+       st.markdown(
+           f"""
+    <div style='
+        background-color: #ffeeba;
+        padding: 10px;
+        border-radius: 5px;
+        color: #856404;
+        font-weight: bold;
+    '>
+        Por favor, escribe tu nombre.
+    </div>
+       """, unsafe_allow_html=True)
